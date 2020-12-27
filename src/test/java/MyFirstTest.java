c93dcde81286f9993b2bd20c81a0f32bd7359ad8
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-27 13:49:49 +0200

    testng dependency to pom file

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,9 +1,19 @@
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
 
-    @Test(testName = "Первый тест")
-    public void myFirstTest(){
+    @BeforeMethod
+    public void prepareFixture(){
+        System.out.println("1");
+    }
 
+    @Test(testName = "Первый тест",description = "Имя первого теста")
+    public void myFirstTest(){
+        System.out.println("2");
+    }
+    @Test(testName = "Второй тест",description = "Имя второго теста")
+    public void mySecondTest(){
+        System.out.println("3");
     }
 }
