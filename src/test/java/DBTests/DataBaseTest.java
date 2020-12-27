2402a5a88ab00dcde36550f73067944a5e7f76b2
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-27 20:28:41 +0200

    lesson 2 -DB connection and requests

--- src/test/java/DBTests/DataBaseTest.java
+++ src/test/java/DBTests/DataBaseTest.java
@@ -31,7 +31,7 @@ public class DataBaseTest {
         role.setName("Пользователь1");
         Role dataBaseRole=RoleRequests.getRole(role);
         Assert.assertEquals(dataBaseRole.getId().intValue(),11);
-        role.setId(15);
+        role.setId(11);
         Role dataBaseRole2=RoleRequests.getRole(role);
         Assert.assertEquals(dataBaseRole2.getName(),"Пользователь1");
     }
