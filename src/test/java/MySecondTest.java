50223f1a91a3303c94205dd7890f04d782ae9199
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-27 14:27:20 +0200

    firsts tests-before

--- src/test/java/MySecondTest.java
+++ src/test/java/MySecondTest.java
@@ -1,8 +1,18 @@
 import org.testng.annotations.Test;
 
 public class MySecondTest extends AbstractTest {
+    /**
+     * Исполнение по порядку не гарантировано-по алфавиту
+     * У афтеров логика такая же только в обратном порядке,так как после
+     */
     @Test(testName = "Мой третий тест", description = "Мой третий тест")
     public void myThirdTest() {
         System.out.println("3");
     }
+
+    @Test(testName = "Мой четвёртый  тест", description = "Мой четвёртый тест")
+    public void myFourthTest() {
+        System.out.println("4");
+    }
+
 }
