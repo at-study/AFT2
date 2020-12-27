09d70bcc6dacbdb51dd18058464349ea6ebd7ba4
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-27 15:58:49 +0200

    TestNG finished

--- src/test/java/testNG/AbstractTest.java
+++ src/test/java/testNG/AbstractTest.java
@@ -1,3 +1,5 @@
+package testNG;
+
 import org.testng.annotations.*;
 
 public class AbstractTest {
--- src/test/java/testNG/MySecondTest.java
+++ src/test/java/testNG/MySecondTest.java
@@ -1,3 +1,5 @@
+package testNG;
+
 import org.testng.annotations.Test;
 
 public class MySecondTest extends AbstractTest {
