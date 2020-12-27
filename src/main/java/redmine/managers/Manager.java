2833f977a0b51cd4073bfcf272b7fe1e1eac8cfa
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-27 19:13:38 +0200

    lesson 2 -DB connection and requests

--- src/main/java/redmine/managers/Manager.java
+++ src/main/java/redmine/managers/Manager.java
@@ -4,11 +4,6 @@ import redmine.db.DataBaseConnection;
 
 public class Manager {
 
-    public static DataBaseConnection dbConnection = new DataBaseConnection();
-
-    public static DataBaseConnection getConnection(){
-        return dbConnection;
-    }
-
+    public final static DataBaseConnection dbConnection = new DataBaseConnection();
 
 }
