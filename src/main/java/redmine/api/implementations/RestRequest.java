6285afa4f5321b7b5dde017a3044c4a57b45571a
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-28 20:14:18 +0200

    user creation with invalid password

--- src/main/java/redmine/api/implementations/RestRequest.java
+++ src/main/java/redmine/api/implementations/RestRequest.java
@@ -1,5 +1,6 @@
 package redmine.api.implementations;
 
+import lombok.Getter;
 import redmine.Property;
 import redmine.api.interfaces.HttpMethods;
 import redmine.api.interfaces.Request;
@@ -8,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+@Getter
 public class RestRequest implements Request {
 
     private String uri;
@@ -27,30 +29,5 @@ public class RestRequest implements Request {
         if(headers==null){ headers=new HashMap<>();}
         this.headers = headers;
         this.body=body;
-    }
-
-    @Override
-    public String getUri() {
-        return uri;
-    }
-
-    @Override
-    public HttpMethods getMethod() {
-        return method;
-    }
-
-    @Override
-    public Map<String, String> getParameters() {
-        return parameters;
-    }
-
-    @Override
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    @Override
-    public Object getBody() {
-        return body;
     }
 }
