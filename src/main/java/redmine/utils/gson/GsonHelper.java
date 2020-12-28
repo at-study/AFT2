5dfef0ffb54b3e410a0b0c7bb3b7513e0f978269
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-28 18:24:49 +0200

    local date time gson

--- src/main/java/redmine/utils/gson/GsonHelper.java
+++ src/main/java/redmine/utils/gson/GsonHelper.java
@@ -4,10 +4,15 @@ import com.google.gson.GsonBuilder;
 import java.time.LocalDateTime;
 
 public class GsonHelper {
+    /**
+     * получение джейсонумеющего работать с локалдейттайм
+     * @return
+     */
     public static Gson getGson(){
         Gson gson= new GsonBuilder()
                 .registerTypeAdapter(LocalDateTime.class,new LocalDateTimeSerializer())
                 .registerTypeAdapter(LocalDateTime.class,new LocalDateTimeDeserializer())
                 .create();
+        return gson;
     }
 }
