50704578e16d6bfa3fd1a429be33ca2d08da3dcb
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-28 20:10:54 +0200

    user creation with invalid password

---
+++ src/main/java/redmine/api/implementations/RestRequest.java
@@ -0,0 +1,56 @@
+package redmine.api.implementations;
+
+import redmine.Property;
+import redmine.api.interfaces.HttpMethods;
+import redmine.api.interfaces.Request;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class RestRequest implements Request {
+
+    private String uri;
+    private HttpMethods method;
+    private Map<String,String> parameters;
+    private Map<String,String> headers;
+    private Object body;
+
+    public RestRequest(String uri, HttpMethods method, Map<String, String> parameters, Map<String, String> headers, Object body) {
+        Objects.requireNonNull(uri,"В запросе должен быть uri");
+        Objects.requireNonNull(method,"В запросе должен быть указан метод запроса");
+        String baseUri= Property.getStringProperty("apiHost");
+        this.uri = baseUri+uri;
+        this.method = method;
+        if(parameters==null){ parameters=new HashMap<>();}
+        this.parameters = parameters;
+        if(headers==null){ headers=new HashMap<>();}
+        this.headers = headers;
+        this.body=body;
+    }
+
+    @Override
+    public String getUri() {
+        return uri;
+    }
+
+    @Override
+    public HttpMethods getMethod() {
+        return method;
+    }
+
+    @Override
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    @Override
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    @Override
+    public Object getBody() {
+        return body;
+    }
+}
