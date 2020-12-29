e55012113c619898942dd6fa87a00975a7de8ee4
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-12-29 17:00:16 +0200

    after 3 with 2 error

--- src/main/java/redmine/model/user/Status.java
+++ src/main/java/redmine/model/user/Status.java
@@ -1,12 +1,12 @@
 package redmine.model.user;
-
 import lombok.*;
-
 @Getter
+@AllArgsConstructor
 
-@NoArgsConstructor
 public enum Status {
-    //  1("активен"),
-    //  2("зарегистрирован"),
-    //  3("заблокирован");
+  ACTIVE(1),
+  NON_ACTIVE(2),
+  LOCKED(3);
+
+  public final Integer status;
 }
