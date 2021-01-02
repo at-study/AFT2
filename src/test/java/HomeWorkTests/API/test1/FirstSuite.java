27c5e74635a2e033a54696780bc66609a69ec229
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-02 21:28:36 +0200

    test 0-connection test

--- src/test/java/HomeWorkTests/API/test1/FirstSuite.java
+++
@@ -1,7 +0,0 @@
-package HomeWorkTests.API.suiteOne;
-/**
- * user name=evgenyttadmin
- * apikey="f2b07eec53f92b54a8522488ca25491167419076"
- */
-public class FirstSuite {
-}
--- src/test/java/HomeWorkTests/API/test2/SecodSuite.java
+++
@@ -1,7 +0,0 @@
-package HomeWorkTests.API.test2;
-
-/**
- * user -evgenyttuser ;api=5aed704a56f9c2711d4cf2035a2d28a698b0cca1
- */
-public class SecodSuite {
-}
