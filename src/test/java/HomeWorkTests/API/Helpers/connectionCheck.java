6b188b09d7a1b43d1201ed74cad39f61d195baef
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-02 18:49:31 +0200

    test 0-connection test

--- src/test/java/HomeWorkTests/API/Helpers/connectionCheck.java
+++ src/test/java/HomeWorkTests/API/Helpers/connectionCheck.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 public class connectionCheck {
     String apiKey = "f2b07eec53f92b54a8522488ca25491167419076";
 
-    @Test(testName = "Запрос проверки на соеденение users-code 200")
+    @Test(testName = "Запрос проверки на соеденение users-code 200" ,priority = 1,description = "Вспомогательный запрос")
     public void restRequestUsersConnectionTest() {
         given().baseUri("http://edu-at.dfu.i-teco.ru/")
                 .contentType(ContentType.JSON)
