fa5cc618bae925c1bc202a5bd56193a483e9b12e
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-04 20:57:11 +0200

    update user making2

--- src/test/java/HOMETESTS/API/TestCase2.java
+++ src/test/java/HOMETESTS/API/TestCase2.java
@@ -14,20 +14,15 @@ public class TestCase2 {
     public void userCreationByNonAdmin() {
         String apiKey = "5aed704a56f9c2711d4cf2035a2d28a698b0cca1";
         String login = randomEnglishLowerString(8);
-        String firstName = randomEnglishLowerString(12);
-        String lastName = randomEnglishLowerString(12);
         String mail = randomEmail();
         String password = String.valueOf(new Random().nextInt(500000) + 100000);
         String body = String.format("{\n" +
                 " \"user\":{\n" +
                 " \"login\":\"%s\",\n" +
-                " \"firstname\":\"%s\",\n" +
-                " \"lastname\":\"%s\",\n" +
                 " \"mail\":\"%s\",\n" +
                 " \"password\":\"%s\" \n" +
                 " }\n" +
-                "}", login, firstName, lastName, mail, password);
-
+                "}", login, mail, password);
         Response response = given().baseUri("http://edu-at.dfu.i-teco.ru/")
                 .contentType(ContentType.JSON)
                 .header("X-Redmine-API-Key", apiKey)
