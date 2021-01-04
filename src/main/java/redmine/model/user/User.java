88b50905f42c5836b6eb818dbe437f2b6731567c
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-04 20:01:20 +0200

    update user making

--- src/main/java/redmine/model/user/User.java
+++ src/main/java/redmine/model/user/User.java
@@ -46,7 +46,7 @@ public class User implements Generatable<User> {
     /**
      * custom user for restapi client generateRandomString(40,"0..f");
      */
-    public String getApiKey() {
+    public static String getApiKey() {
         //TODO  Изменить на генерацию ключа API
         return "f02b2da01a468c4116be898911481d1b928c15f9";
     }
