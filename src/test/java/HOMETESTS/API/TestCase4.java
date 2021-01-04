46de52a31402333d58ac700073394f9941bedccd
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-04 16:29:03 +0200

    +названия тестов и методов для 3и 4

--- src/test/java/HOMETESTS/API/TestCase4.java
+++ src/test/java/HOMETESTS/API/TestCase4.java
@@ -1,8 +1,39 @@
 package HOMETESTS.API;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import redmine.api.implementations.RestApiClient;
+import redmine.api.implementations.RestRequest;
+import redmine.api.interfaces.ApiClient;
+import redmine.api.interfaces.HttpMethods;
+import redmine.api.interfaces.Request;
+import redmine.db.requests.RoleRequests;
+import redmine.db.requests.UserRequests;
+import redmine.model.role.Role;
+import redmine.model.user.User;
 
-/**
- * f02b2da01a468c4116be898911481d1b928c15f9
- * 5f53e117604928097361205d1bba409b5c6211a4
- */
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+import static redmine.utils.StringGenerators.randomEmail;
+import static redmine.utils.StringGenerators.randomEnglishLowerString;
+
+
 public class TestCase4 {
+
+    @Test(testName = "Шаг 1-Удаление пользователя другим пользователем и проверка в бд ")
+    public void userDeleteByOtherUser(){
+        String apiKeyUserOne="f02b2da01a468c4116be898911481d1b928c15f9";
+        String apiKeyUserTwo="5f53e117604928097361205d1bba409b5c6211a4";
+
+    }
+    @Test(testName = "Шаг 2-Удаление пользователя самим собою и проверка в бд ")
+    public void userDeleteByHimself(){
+        String apiKeyUserOne="f02b2da01a468c4116be898911481d1b928c15f9";
+        String apiKeyUserTwo="5f53e117604928097361205d1bba409b5c6211a4";
+
+    }
 }
