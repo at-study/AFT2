afb5a45a2d6f4e31db4de6cca93a5451d92bd8fe
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-04 21:13:06 +0200

    update user making2

--- src/main/java/redmine/db/requests/UserRequests.java
+++ src/main/java/redmine/db/requests/UserRequests.java
@@ -17,13 +17,12 @@ public class UserRequests {
     public static User updateUser(User user) {
         String query = """
                 update public.users
-                set hashed_password=?,firstname=?,lastname=?,admin=?,status=?,last_login_on=?,language=?,
-                auth_source_id=?,created_on=?,type=?,identity_url=?,mail_notification=?,salt=?,must_change_passwd=?,
-                passwd_changed_on=? where login=? RETURNING id;
+                set hashed_password=?,firstname=?,lastname=?,admin=?,status=?,language=?,
+                where login=? RETURNING id;
                 """;
         List<Map<String, Object>> result = Manager.dbConnection.executePreparedQuery(query,
                 user.getHashed_password(), user.getFirstname(), user.getLastname(), user.getAdmin().toString(),
-                user.getStatus().toString());
+                user.getStatus().toString(),user.getLanguage().toString(),user.getLogin());
         user.setId((Integer) result.get(0).get("id"));
         return user;
     }
