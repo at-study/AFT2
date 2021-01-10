2712dd760d2e6a2f27b09787557bf7b1b5b560f7
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-10 17:18:19 +0200

    after 7

--- src/test/java/HOMETESTS/API/TestCase3.java
+++ src/test/java/HOMETESTS/API/TestCase3.java
@@ -7,13 +7,11 @@ public class TestCase3 {
     public void userInfoAboutHimself() {
         String apiKeyUserOne = "f02b2da01a468c4116be898911481d1b928c15f9";
         String apiKeyUserTwo = "5f53e117604928097361205d1bba409b5c6211a4";
-
     }
 
     @Test(testName = "Шаг 2-Получение пользователем инфо о другом пользователе +допинфо  ")
     public void userInfoAboutOtherUser() {
         String apiKeyUserOne = "f02b2da01a468c4116be898911481d1b928c15f9";
         String apiKeyUserTwo = "5f53e117604928097361205d1bba409b5c6211a4";
-
     }
 }
