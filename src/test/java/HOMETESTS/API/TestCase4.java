707b3d2da6eabac0cd7104cdda541a626ae2aac3
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-12 17:15:36 +0200

    осталось 5 жёлтых и 3 красных

--- src/test/java/HOMETESTS/API/TestCase4.java
+++ src/test/java/HOMETESTS/API/TestCase4.java
@@ -1,4 +1,6 @@
 package HOMETESTS.API;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,6 +14,7 @@ import redmine.model.Dto.UserDto;
 import redmine.model.user.User;
 import redmine.utils.gson.GsonHelper;
 
+import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 import static redmine.utils.StringGenerators.randomEmail;
 import static redmine.utils.StringGenerators.randomEnglishLowerString;
@@ -79,39 +82,15 @@ public class TestCase4 {
         System.out.println("Created userID 2: "+userIdSecondUser);
     }
 
-    @Test(testName = "Шаг 2-Удаление пользователя самим собою и проверка в бд ")
+    @Test(testName = "Шаг 2(без проверки) -Удаление пользователя самим собою и проверка в бд ")
     public void userDeleteByHimself() {
-        String login = randomEnglishLowerString(8);
-        String firstName = randomEnglishLowerString(12);
-        String lastName = randomEnglishLowerString(12);
-        String mail = randomEmail();
-        Integer status = 2;
-        String body = String.format("""
-                {
-                 "user":{
-                 "login":"%s",
-                 "firstname":"%s",
-                 "lastname":"%s",
-                 "mail":"%s",
-                 "status":"%s",
-                 "password":"1qaz@WSX"\s
-                 }
-                }""", login, firstName, lastName, mail, status);
-        ApiClient apiClient = new RestApiClient(user);
-        Request createRequest = new RestRequest("users.json", HttpMethods.POST, null, null, body);
-        Response createResponse = apiClient.executeRequest(createRequest);
-        Assert.assertEquals(createResponse.getStatusCode(), 201);
-        UserDto createdInfoUser = createResponse.getBody(UserDto.class);
-        Assert.assertNotNull(createdInfoUser.getUser().getId());
-        Integer userId=createdInfoUser.getUser().getId();
-        String userApiKey=createdInfoUser.getUser().getApi_key();
-        System.out.println("Created userID 1: "+userId);
-
+        Integer userId=725;
+        String userApiKey="5aed704a56f9c2711d4cf2035a2d28a698b0cca1";
         String uri = String.format("users/%d.json",userId);
-        Request getRequest = new RestRequest(uri, HttpMethods.GET, null, null, null);
-        Response getResponse = apiClient.executeRequest(getRequest);
-        assertEquals(getResponse.getStatusCode(), 200);
-        String responseBody = getResponse.getBody().toString();
-        UserDto createdGetUser = GsonHelper.getGson().fromJson(responseBody, UserDto.class);
+        io.restassured.response.Response deleteResponse = given().baseUri("http://edu-at.dfu.i-teco.ru/")
+                .contentType(ContentType.JSON)
+                .header("X-Redmine-API-Key", userApiKey)
+                .request(Method.DELETE, uri);
+        Assert.assertEquals(deleteResponse.getStatusCode(), 403);
     }
 }
