324f6f1f1060dfd3ab9ad5ca71bb427d172ff498
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-21 19:50:10 +0200

    test 2 +4 ready

--- src/test/java/hometests/api/TestCase4.java
+++ src/test/java/hometests/api/TestCase4.java
@@ -10,22 +10,24 @@ import redmine.model.user.User;
 import static io.restassured.RestAssured.given;
 
 public class TestCase4 {
-    User user;
+    User firstUser;
+    User secondUser;
 
     @BeforeMethod
     public void prepareFixtures() {
-        user = new User().generate();
+        firstUser = new User().setAdmin(false).setStatus(1).generate();
+        secondUser = new User().setAdmin(false).setStatus(1).generate();
     }
 
     @Test(testName = "Шаг 1-Удаление пользователя другим пользователем и проверка в бд ",priority = 5)
     public void userDeleteByOtherUser() {
-        String userApiKey = "5aed704a56f9c2711d4cf2035a2d28a698b0cca1";
-        Integer secondUserId = 726;
+        String firstUserApiKey = firstUser.getApiKey();
+        Integer secondUserId = secondUser.getId();
         String uri = String.format("users/%d.json", secondUserId);
         int usersBeforeDelete = UserRequests.getAllUsers().size();
         io.restassured.response.Response deleteResponse = given().baseUri("http://edu-at.dfu.i-teco.ru/")
                 .contentType(ContentType.JSON)
-                .header("X-Redmine-API-Key", userApiKey)
+                .header("X-Redmine-API-Key", firstUserApiKey)
                 .request(Method.DELETE, uri);
         Assert.assertEquals(deleteResponse.getStatusCode(), 403);
         int userCountAfterDelete = UserRequests.getAllUsers().size();
@@ -34,8 +36,8 @@ public class TestCase4 {
 
     @Test(testName = "Шаг 2 -Удаление пользователя самим собою и проверка в бд ",priority = 10)
     public void userDeleteByHimself() {
-        Integer userId = 725;
-        String userApiKey = "5aed704a56f9c2711d4cf2035a2d28a698b0cca1";
+        Integer userId = firstUser.getId();
+        String userApiKey = firstUser.getApiKey();
         String uri = String.format("users/%d.json", userId);
         int usersBeforeDelete = UserRequests.getAllUsers().size();
         io.restassured.response.Response deleteResponse = given().baseUri("http://edu-at.dfu.i-teco.ru/")
