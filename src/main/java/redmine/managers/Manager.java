d44f082e22f488d648930f3e4e58fb45896bbeba
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-22 19:00:06 +0200

    afterlesson5

--- src/main/java/redmine/managers/Manager.java
+++ src/main/java/redmine/managers/Manager.java
@@ -2,6 +2,7 @@ package redmine.managers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import redmine.Property;
 import redmine.db.DataBaseConnection;
 
@@ -13,8 +14,7 @@ public class Manager {
 
     public static WebDriver driver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", Property.getStringProperty("webdriver.chrome.driver"));
-            driver = new ChromeDriver();
+            driver=getPropertyDriver();
             driver.manage().window().maximize();
         }
         return driver;
@@ -29,4 +29,18 @@ public class Manager {
         driver().get(Property.getStringProperty("ui.url")+uri);
     }
 
+    private static WebDriver getPropertyDriver(){
+       switch( Property.getStringProperty("browser")){
+           case "chrome":
+               System.setProperty("webdriver.chrome.driver", Property.getStringProperty("webdriver.chrome.driver"));
+               driver = new ChromeDriver();
+               return new ChromeDriver();
+           case "firefox":
+               System.setProperty("webdriver.gecko.driver", Property.getStringProperty("webdriver.gecko.driver"));
+               driver = new FirefoxDriver();
+               return new FirefoxDriver();
+           default:
+               throw new IllegalArgumentException("Неизвестный тип браузера (добавить в Менеджер и Properties !)");
+       }
+    }
 }
--- src/main/java/redmine/ui/pages/LoginPage.java
+++ src/main/java/redmine/ui/pages/LoginPage.java
@@ -3,6 +3,7 @@ package redmine.ui.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 
 public  class LoginPage extends AbstractPage {
     private WebDriver driver;
@@ -16,10 +17,17 @@ public  class LoginPage extends AbstractPage {
     @FindBy(xpath = "//input[@id='login-submit']")
     private WebElement submitButton;
 
+    @FindBy(xpath = "//div[@id='flash_error']")
+    private WebElement flashError;
+
     public void login(String login, String password) {
         loginElement.sendKeys(login);
         passwordElement.sendKeys(password);
         submitButton.click();
+    }
+
+    public String errorMessage(){
+        return flashError.getText();
     }
 
 }
