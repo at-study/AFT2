222c579290d766062c010940b379b35818e082d3
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-22 14:59:19 +0200

    1 test as admin -loggedas

--- src/test/java/hometests/ui/MyFirstTest.java
+++ src/test/java/hometests/ui/MyFirstTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -24,6 +25,12 @@ public class MyFirstTest {
         WebElement  passwordElement=driver.findElement(By.xpath("//input[@id='password']"));
         WebElement  submitElement=driver.findElement(By.xpath("//input[@id='login-submit']"));
 
+        loginElement.sendKeys("admin");
+        passwordElement.sendKeys("admin123");
+        submitElement.click();
+
+        WebElement loggedAs=driver.findElement(By.xpath("//div[@id='loggedas']"));
+        Assert.assertEquals(loggedAs.getText(),"Вошли как admin");
     }
     @AfterMethod
     public void tearDown(){
