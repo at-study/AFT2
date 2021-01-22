0bc92717178d2f2a6a0095201e22b063dc4b2ec9
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-22 17:13:31 +0200

    PageFactory2

--- src/main/java/redmine/ui/pages/HeaderPage.java
+++ src/main/java/redmine/ui/pages/HeaderPage.java
@@ -1,11 +1,9 @@
 package redmine.ui.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import java.util.Objects;
 
 public class HeaderPage {
