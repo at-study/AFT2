cc51ff3a453b3a3c4022281994180bd0c15416f6
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-22 14:44:08 +0200

    tearDown

--- src/test/java/hometests/ui/MyFirstTest.java
+++ src/test/java/hometests/ui/MyFirstTest.java
@@ -2,6 +2,7 @@ package hometests.ui;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -17,5 +18,9 @@ public class MyFirstTest {
     @Test
     public void myFirstLoginTest() {
 
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 }
