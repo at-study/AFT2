768a788363826cbcc8a397cce236c9e538801c9a
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-22 14:43:01 +0200

    selenium chromiun driver

--- src/test/java/hometests/ui/MyFirstTest.java
+++ src/test/java/hometests/ui/MyFirstTest.java
@@ -2,11 +2,20 @@ package hometests.ui;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void prepareFixture(){
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void myFirstLoginTest() {
-        WebDriver driver = new ChromeDriver();
+
     }
 }
