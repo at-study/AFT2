e636f81fef804de0a9e283738cad5658719b64a7
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-22 17:12:11 +0200

    PageFactory2

--- src/main/java/redmine/ui/pages/HeaderPage.java
+++ src/main/java/redmine/ui/pages/HeaderPage.java
@@ -3,18 +3,24 @@ package redmine.ui.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Objects;
 
 public class HeaderPage {
     private WebDriver driver;
+    @FindBy(xpath="//a[@class='home']")
     private WebElement home;
+    @FindBy(xpath="//a[@class='projects']")
     private WebElement projects;
+    @FindBy(xpath="//div[@id='loggedas']")
     private WebElement loggeAs;
 
     public HeaderPage(WebDriver driver){
+        Objects.requireNonNull(driver);
         this.driver=driver;
-        home=driver.findElement(By.xpath("//a[@class='home']"));;
-        projects=driver.findElement(By.xpath("//a[@class='projects']"));
-        loggeAs=driver.findElement(By.xpath("//div[@id='loggedas']"));
+        PageFactory.initElements(driver,this);
     }
 
     public String loggedAs(){
