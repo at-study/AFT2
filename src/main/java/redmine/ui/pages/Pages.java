de16178c39d1e6d84336c618d8b2084e6bd727ea
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-25 18:54:06 +0200

    priority api

--- src/main/java/redmine/ui/pages/Pages.java
+++ src/main/java/redmine/ui/pages/Pages.java
@@ -1,5 +1,6 @@
 package redmine.ui.pages;
 
+import io.qameta.allure.Allure;
 import lombok.SneakyThrows;
 import org.openqa.selenium.support.PageFactory;
 import redmine.managers.Manager;
@@ -8,8 +9,10 @@ public class Pages {
 
     @SneakyThrows
     public static <T extends AbstractPage> T getPage(Class<T> clazz) {
-        T page = clazz.newInstance();
-        PageFactory.initElements(Manager.driver(), page);
-        return page;
+        return Allure.step("Обращение к странице" + clazz.getSimpleName(), () -> {
+            T page = clazz.newInstance();
+            PageFactory.initElements(Manager.driver(), page);
+            return page;
+        });
     }
 }
