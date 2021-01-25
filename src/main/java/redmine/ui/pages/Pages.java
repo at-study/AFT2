b3e7a41bfc23f15536c88797d790a07d5096aa95
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-25 20:47:14 +0200

    end for 8

--- src/main/java/redmine/ui/pages/Pages.java
+++ src/main/java/redmine/ui/pages/Pages.java
@@ -1,5 +1,6 @@
 package redmine.ui.pages;
 
+import io.qameta.allure.Allure;
 import lombok.SneakyThrows;
 import org.openqa.selenium.support.PageFactory;
 import redmine.managers.Manager;
@@ -8,6 +9,7 @@ public class Pages {
 
     @SneakyThrows
     public static <T extends AbstractPage> T getPage(Class<T> clazz) {
+        Allure.step("Обращение к странице "+clazz.getSimpleName());
         T page = clazz.newInstance();
         PageFactory.initElements(Manager.driver(), page);
         return page;
