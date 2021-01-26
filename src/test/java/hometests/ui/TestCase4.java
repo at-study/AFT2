7cbe4234fb8f2182e233564d4ee1c54995f286cf
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-26 17:03:55 +0200

    Asserts

--- src/test/java/hometests/ui/TestCase4.java
+++ src/test/java/hometests/ui/TestCase4.java
@@ -1,7 +1,6 @@
 package hometests.ui;
-import static redmine.utils.Asserts.assertEquals;
+
 import io.qameta.allure.Description;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,7 +10,6 @@ import redmine.ui.pages.HeaderPage;
 import redmine.ui.pages.LoginPage;
 import redmine.ui.pages.ProjectsPage;
 import redmine.utils.Asserts;
-
 import static redmine.managers.Manager.*;
 import static redmine.ui.pages.Pages.getPage;
 
