f8940afd154aa7b1035d1f52866830f37ff2338c
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-02-05 00:21:53 +0200

    cucumber first ready

--- src/main/java/redmine/ui/pages/helpers/CucumberPageObjectHelper.java
+++ src/main/java/redmine/ui/pages/helpers/CucumberPageObjectHelper.java
@@ -4,7 +4,6 @@ import lombok.SneakyThrows;
 import org.openqa.selenium.WebElement;
 import org.reflections.Reflections;
 import redmine.ui.pages.AbstractPage;
-import redmine.ui.pages.HeaderPage;
 
 import java.lang.reflect.Field;
 import java.util.Set;
@@ -12,25 +11,25 @@ import java.util.stream.Stream;
 
 public class CucumberPageObjectHelper {
     @SneakyThrows
-    public static WebElement getElementBy(String cucumberFieldName) {
-        HeaderPage page = Pages.getPage(HeaderPage.class);
-        Field founfField = Stream.of(page.getClass().getDeclaredFields())
+    public static WebElement getElementBy(String cucumberPageName,String cucumberFieldName) {
+        AbstractPage page = getPageBy(cucumberPageName);
+        Field foundField = Stream.of(page.getClass().getDeclaredFields())
                 .filter(field -> field.isAnnotationPresent(CucumberName.class))
                 .filter(field -> cucumberFieldName.equals(field.getAnnotation(CucumberName.class).value()))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException(String.format("Нет аннотации @CucumberName(\"%s\")", cucumberFieldName)));
-        founfField.setAccessible(true);
-        return (WebElement) founfField.get(page);
+        foundField.setAccessible(true);
+        return (WebElement) foundField.get(page);
     }
 
     @SneakyThrows
-    public static AbstractPage getPageBy(String cucumberPageName) {
+    private static AbstractPage getPageBy(String cucumberPageName) {
         Reflections reflections = new Reflections("my.project.prefix");
         Set<Class<?>> allClasses = reflections.getTypesAnnotatedWith(CucumberName.class);
 
         Class<?> pageClass = allClasses.stream().filter(clazz -> cucumberPageName.equals(clazz.getAnnotation(CucumberName.class).value()))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException(String.format("Нет аннотации @CucumberName(\"%s\") у класса", cucumberPageName)));
-        return Pages.getPage((Class<AbstractPage>) pageClass);
+        return (AbstractPage) pageClass.newInstance();
     }
 }
--- src/test/java/hometests/steps/ZAssertionSteps.java
+++ src/test/java/hometests/steps/ZAssertionSteps.java
@@ -1,6 +1,8 @@
 package hometests.steps;
 
+import cucumber.api.java.ru.И;
 import cucumber.api.java.ru.То;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import redmine.managers.Context;
 import redmine.ui.pages.helpers.CucumberPageObjectHelper;
@@ -15,11 +17,16 @@ public class ZAssertionSteps {
 
     @То("На главной странице отображается поле {string}")
     public void assertProjectElementIsDisplayed(String fieldName) {
-        Assert.assertTrue(BrowserUtils.isElementCurrentlyPresent(CucumberPageObjectHelper.getElementBy(fieldName)));
+        Assert.assertTrue(BrowserUtils.isElementCurrentlyPresent(CucumberPageObjectHelper.getElementBy("Заголовок",fieldName)));
     }
 
     @То("На главной странице не отображается поле {string}")
     public void assertProjectElementIsNotDisplayed(String fieldName) {
-        Assert.assertFalse(BrowserUtils.isElementCurrentlyPresent(CucumberPageObjectHelper.getElementBy(fieldName)));
+        Assert.assertFalse(BrowserUtils.isElementCurrentlyPresent(CucumberPageObjectHelper.getElementBy("Заголовок",fieldName)));
+    }
+
+    @И("На странице {string} отображается элемент {string}")
+    public void assertFieldIsDisplayed(String pageName,String fieldName){
+        WebElement element=CucumberPageObjectHelper.getElementBy(pageName,fieldName);
     }
 }
