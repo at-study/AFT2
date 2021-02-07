7b842a5d0caac2e570e376930b689a4fce7d200b
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-02-07 15:49:39 +0200

    первые тесты почти до конца

--- src/test/java/hometests/steps/RequestSteps.java
+++ src/test/java/hometests/steps/RequestSteps.java
@@ -2,10 +2,15 @@ package hometests.steps;
 
 import cucumber.api.java.ru.Если;
 import redmine.api.interfaces.Response;
+import redmine.managers.Context;
+import redmine.model.user.User;
 
-public class RequestSteps {
+public  class RequestSteps {
     @Если("Отправить запрос на создание пользователя {string} с параметрами:")
-    public Response answerOnUserCreationRequest(){
+    public static Response answerOnUserCreationRequest(String userStashId){
+        User admin = Context.get(userStashId, User.class);
+        String apiKeyForQuery=admin.getApiKey();
+        System.out.println(apiKeyForQuery);
         return null;
     }
 }
