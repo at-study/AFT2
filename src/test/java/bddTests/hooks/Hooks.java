ae077402c0145fd4f99644cfbe0734de40da7b67
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-02-13 15:27:18 +0200

    save stash

---
+++ src/test/java/bddTests/hooks/Hooks.java
@@ -0,0 +1,12 @@
+package bddTests.hooks;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import redmine.managers.Context;
+
+public class Hooks {
+    @After
+    public void afterAll(Scenario scenario){
+        Context.saveStashToAllure();
+    }
+}
